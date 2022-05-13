ecc4678675b027ed86c0b55b7db56fb4259d6785
Adynaton44 <giacomoroggero@gmail.com>
2022-05-13 11:52:15 +0200

    Edits to professors

---
+++ api/dataObjects/Professor.php
@@ -0,0 +1,140 @@
+<?php
+//id name surname
+
+class Professor implements CRUDL, ASerializable//check
+{
+    private int $id;
+    private string $name, $surname;
+    private int $created, $lastEdit;
+
+    function __construct(PDO $database, ?LoggedInUser $loggedInUser)
+    {
+        $this->database = $database;
+        $this->loggedInUser = $loggedInUser;
+    }
+    public function get(int $id): void
+    {
+        $q = $this->database->prepare("SELECT * FROM Professors WHERE id=:id");
+        $q->execute([":id" => $id]);
+        $r = $q->fetch(PDO::FETCH_ASSOC);
+        if (!$r) throw new NotFoundException();
+        $this->id = $id;
+        $this->name = $r["name"];
+        $this->surname = $r["surname"];
+        $this->created = (int) $r["created"];
+        $this->lastEdit = (int) $r["lastEdit"];
+
+    }
+    public function list(): array
+    {
+        $q = $this->database->prepare("SELECT * FROM Professors");
+        $q->execute();
+        $re = $q->fetchAll(PDO::FETCH_ASSOC);
+        $final = [];
+        foreach ($re as $result) {
+            $u = new Professor($this->database, $this->loggedInUser);
+            $u->deserialize($result);
+            $final[] = $u;
+        }
+        return $final;
+    }
+    public function delete(): void
+    {
+        if (!$this->loggedInUser) throw new AuthErrorException();
+        if ($this->loggedInUser->getUser()->getId() != $this->id) throw new UnauthorizedException();
+        $p = $this->database->prepare("DELETE FROM Professors WHERE id = :id");
+        $p->execute([":id" => $this->id]);
+        //cascade delete descriptor from all games and snaicards
+
+        $q = $this->database->prepare("SELECT id, professorIds FROM Games WHERE professorIds LIKE ('%' || ',' || trim(lower(:did)) || ',' || '%') OR professorIds LIKE (trim(lower(:did)) || ',' || '%') OR professorIds LIKE ('%' || ',' || trim(lower(:did)) ) ");
+        /**
+         * how the descriptorIds is searched?
+         * "a,..."
+         * "...,a,..."
+         * "...,a"
+         */
+
+        $q->execute([
+            ":did" => (string) $this->id
+        ]);
+        
+        $re = $q->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($re as $result) {
+            $ar = explode(",", $result["professorIds"]);
+            if (($key = array_search($this->id, $ar)) !== false) {
+                unset($ar[$key]);
+            }
+            $q = $this->database->prepare("UPDATE Games SET professorIds = :did WHERE id=:id");
+            $q->execute([
+                ":did" => implode(",", $ar),
+                ":id" => $this->id
+            ]);
+        }
+
+        $q = $this->database->prepare("SELECT id, bettedProfsIds FROM SNAICards WHERE bettedProfsIds LIKE ('%' || ',' || trim(lower(:did)) || ',' || '%') OR bettedProfsIds LIKE (trim(lower(:did)) || ',' || '%') OR bettedProfsIds LIKE ('%' || ',' || trim(lower(:did)) ) ");
+        /**
+         * how the descriptorIds is searched?
+         * "a,..."
+         * "...,a,..."
+         * "...,a"
+         */
+
+        $q->execute([
+            ":did" => (string) $this->id
+        ]);
+        
+        $re = $q->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($re as $result) {
+            $ar = explode(",", $result["bettedProfsIds"]);
+            if (($key = array_search($this->id, $ar)) !== false) {
+                unset($ar[$key]);
+            }
+            $q = $this->database->prepare("UPDATE SNAICards SET bettedProfsIds = :did WHERE id=:id");
+            $q->execute([
+                ":did" => implode(",", $ar),
+                ":id" => $this->id
+            ]);
+        }
+    }
+    public function update(): void
+    {
+        if (!$this->id) throw new NotFoundException();
+        $q = $this->database->prepare("UPDATE Professors SET name=:name, surname=:surname, lastEdit=:lastEdit WHERE id=:id");
+        $q->execute([
+            ":name" => $this->name,
+            ":surname" => $this->surname,
+            ":lastEdit" => time(),
+            ":id" => $this->id
+        ]);
+    }
+    public function create(array $data): void
+    {
+        $this->deserialize($data);
+        $hash = password_hash($data["password"], PASSWORD_DEFAULT);
+        $q = $this->database->prepare("INSERT INTO Professor(name, surname, lastEdit, created) VALUES( :name, :surname, :lastEdit, :created)");
+        $this->created = time();
+        $this->lastEdit = $this->created;
+        $q->execute([//check
+            ":title" => $this->title,
+            ":description" => $this->description,
+            ":gameMasterId" => $this->gameMasterId,
+            ":maxBettableProfs" => $this->maxBettableProfs,
+            ":start" => $this->start,
+            ":end" => $this->end,
+            ":professorIds" => (string) $this->professorIds,
+            ":descriptorIds" => (string) $this->descriptorIds,
+            ":lastEdit" => $this->lastEdit,
+            ":created" => $this->created,
+            ":id" => $this->id
+        ]);
+        $this->passwordHash = $hash;
+        $this->id = $this->database->lastInsertId();
+    }
+    public function deserialize(array $r): void
+    {
+    }
+    public function serialize(): array
+    {
+        
+    }
+}
